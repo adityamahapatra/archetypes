692364c8b52cd2d1cc5002842d9d73136f8f419d
Aditya Mahapatra <adi.mahapatra@gmail.com>
2023-04-11 17:29:38 +0530

    Add more modules.

--- src/charGenericRobot/AvA/LodA/archtype.py
+++ src/charGenericRobot/AvA/LodA/archtype.py
@@ -1,6 +1,8 @@
-import inspect
-from pprint import pprint
 import sys
 sys.path.append("/home/aditya/Dev/projects")
 
 from componentLibrary.src.charOptimusPrime.arm import v1 as arm
+from componentLibrary.src.charOptimusPrime.leg import v2 as leg
+from componentLibrary.src.charMpcHuman.spine import v3 as spine
+from dotenv import load_dotenv
+import requests
