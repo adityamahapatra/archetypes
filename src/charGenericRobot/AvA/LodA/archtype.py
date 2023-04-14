27d49d25e3a01446f37eee6af50353666a2b00b0
Aditya Mahapatra <adi.mahapatra@gmail.com>
2023-04-14 16:38:43 +0530

    Add placeholder functions & relevant comments.

--- src/charGenericRobot/AvA/LodA/archtype.py
+++ src/charGenericRobot/AvA/LodA/archtype.py
@@ -1,11 +1,23 @@
 import sys
+
+# Temporary arrangement. Installation path would be configured in production.
 sys.path.append("/home/aditya/Dev/projects")
 
+# Third-party library imports.
 from dotenv import load_dotenv
 import requests
 from pandas import DataFrame
 import numpy
 
+# Internal library imports.
 from componentLibrary.src.charOptimusPrime.arm import v1 as arm
 from componentLibrary.src.charOptimusPrime.leg import v2 as leg
 from componentLibrary.src.charMpcHuman.spine import v3 as spine
+
+
+def specs():
+    pass
+
+
+def build():
+    pass
